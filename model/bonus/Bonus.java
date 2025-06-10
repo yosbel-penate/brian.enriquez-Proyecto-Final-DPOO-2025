c6d86e52e6ab5700c967c9c30c56a9356143d6ef
BrianGamerW <54040873+BrianGamerW@users.noreply.github.com>
2025-06-09 21:32:25 -0400

    Add files via upload

---
+++ model/bonus/Bonus.java
@@ -0,0 +1,32 @@
+
+package model.bonus;
+
+import model.Element;
+import model.Position;
+
+public abstract class Bonus extends Element{
+    protected String BonusDescription;
+
+    public Bonus() {
+    }
+
+    public Bonus(String BonusDescription, String name, Position position, String symbol, String description, boolean visibility, String id) {
+        super(name, position, symbol, description, visibility, id);
+        this.BonusDescription = BonusDescription;
+    }
+
+    public Bonus(String BonusDescription) {
+        this.BonusDescription = BonusDescription;
+    }
+
+    public String getBonusDescription() {
+        return BonusDescription;
+    }
+
+    public void setBonusDescription(String BonusDescription) {
+        this.BonusDescription = BonusDescription;
+    }
+
+    public abstract void setParametersInitialDefault();
+}
+
